c409e49dbeb28612226050e74fdddd7435620bc8
Victor Dibia <victordibia@Victors-MBP.fios-router.home>
2016-11-20 16:21:08 -0500

    update photo params

--- tjvision.js
+++ tjvision.js
@@ -1,9 +1,11 @@
 var RaspiCam = require("raspicam");
 var options = {
   mode: "photo",
-  w: 180,
+  w: 480,
+  h: 480,
+  rot: 180,
   output: "bingo.jpg"
 }
 var photo = new RaspiCam(options);
 //to take a snapshot, start a timelapse or video recording
-photo.start( );
+photo.start();
