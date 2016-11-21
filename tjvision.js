523edd34fefbed49f89ba324f64bdc13f7ed43e1
Victor Dibia <victordibia@Victors-MBP.fios-router.home>
2016-11-20 20:29:24 -0500

    grap still photos every X seconds and process

--- tjvision.js
+++ tjvision.js
@@ -24,75 +24,80 @@ var options = {
   output: "shot.jpg"
 }
 
-var snapinterval =  3000 ; // take a picture every X milliseconds
+var snapinterval =  10000 ; // take a picture every X milliseconds
 
 var camera = new RaspiCam(options);
-//to take a snapshot, start a timelapse or video recording
-camera.start();
 
-//listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, filename){
-  //do stuff
-  console.log("image saved ", filename)
-  processImage("shot.jpg")
-});
+
+function launchVision(){
+  setInterval(function () {
+    //to take a snapshot, start a timelapse or video recording
+    camera.start();
+    //listen for the "read" event triggered when each new photo/video is saved
+    camera.on("read", function(err, filename){
+      //do stuff
+      console.log("image saved ", filename)
+      processImage("shot.jpg")
+    });
+  }, snapinterval);
+}
 
 /**
- * [processImage send the given image file to Watson Vision Recognition for Analysis]
- * @param  {[type]} imagefile [description]
- * @return {[type]}           [description]
- */
- function processImage(imagefile){
-   var params = {
-     images_file: fs.createReadStream(imagefile)
-   };
+* [processImage send the given image file to Watson Vision Recognition for Analysis]
+* @param  {[type]} imagefile [description]
+* @return {[type]}           [description]
+*/
+function processImage(imagefile){
+  var params = {
+    images_file: fs.createReadStream(imagefile)
+  };
 
-   var resultstring = "Objects in the image are: " ;
+  var resultstring = "Objects in the image are: " ;
 
-   visual_recognition.classify(params, function(err, res) {
-     if (err){
-       console.log(err);
-     } else {
-       result = res.images[0].classifiers[0].classes
-       if(result !== null & result.length > 0){
-         result.forEach(function(obj){
-           console.log(obj.class)
-           resultstring = resultstring + obj.class + ", "
-         })
+  visual_recognition.classify(params, function(err, res) {
+    if (err){
+      console.log(err);
+    } else {
+      result = res.images[0].classifiers[0].classes
+      if(result !== null & result.length > 0){
+        result.forEach(function(obj){
+          console.log(obj.class)
+          resultstring = resultstring + obj.class + ", "
+        })
 
-         console.log(resultstring)
-         speak(resultstring);
-       }else {
-         resultstring = "I could not understand that image. Try anoter?"
-         console.log(resultstring)
-         speak(resultstring);
-       }
-       //console.log("================\n",JSON.stringify(result), null, 2));
-     }
-   });
- }
+        console.log(resultstring)
+        speak(resultstring);
+      }else {
+        resultstring = "I could not understand that image. Try anoter?"
+        console.log(resultstring)
+        speak(resultstring);
+      }
+      //console.log("================\n",JSON.stringify(result), null, 2));
+    }
+  });
+}
 
 
- var Sound = require('node-aplay');
- var music ;
- var text_to_speech = watson.text_to_speech({
-   username: config.TTSUsername,
-   password: config.TTSPassword,
-   version: 'v1'
- });
+var Sound = require('node-aplay');
+var music ;
+var text_to_speech = watson.text_to_speech({
+  username: config.TTSUsername,
+  password: config.TTSPassword,
+  version: 'v1'
+});
 
- function speak(textstring){
-   var params = {
-     text: textstring,
-     voice: config.voice,
-     accept: 'audio/wav'
-   };
-   text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav')).on('close', function() {
-     // var create_audio = exec('ffplay -autoexit output.wav', function (error, stdout, stderr) { // if on mac
-     music = new Sound("output.wav");
-     music.play();
-     music.on('complete', function () {
-       console.log('Done with playback!');
-     });
-   });
- }
+function speak(textstring){
+  var params = {
+    text: textstring,
+    voice: config.voice,
+    accept: 'audio/wav'
+  };
+  text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav')).on('close', function() {
+    // var create_audio = exec('ffplay -autoexit output.wav', function (error, stdout, stderr) { // if on mac
+    music = new Sound("output.wav");
+    music.play();
+    music.on('complete', function () {
+      console.log('Done with playback!');
+    });
+  });
+}
